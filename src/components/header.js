b5a0da8f362328465b13be63ab2def62f7d039fd
Mariam Apaza Santillana <mariam.apaza@tecsup.edu.pe>
2021-03-29 16:28:23 -0500

    NavBar opciones

--- src/components/header.js
+++ src/components/header.js
@@ -7,28 +7,62 @@ const Header = (props) => {
     return (
 <section class="text-gray-600 body-font">
   <div class="container mx-auto flex px-10 py-10 md:flex-row flex-col items-center">
-    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-5 md:mb-0 items-center text-center">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 font-cuerpo">Bienvenid@ a Meowy Shop!
       </h1>
-      <p class="mb-8 leading-relaxed font-cuerpo">
+      <p class="mb-3 eading-relaxed font-cuerpo">
                 Compra los mejores accesiorios para tus mascotas y recíbelos en la puerta de tu casa
       </p>
-      <div class="flex justify-center">
-        <button class="text-white bg-red-200 border-0 py-2 px-6 focus:outline-none hover:bg-red-300 rounded text-lg font-cuerpo">Regístrate</button>
-        <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg font-cuerpo">Iniciar Sesión</button>
-      </div>
     </div>
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 ">
     <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-    <div class="absolute inset-0 bg-gradient-to-r from-green-100 via-white to-green-200 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-    <div class="relative px-5 py-5 bg-white shadow-lg sm:rounded-3xl sm:p-5">
-      <div class="max-w-md mx-auto">
-        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+    <div class="absolute inset-0 bg-gradient-to-r from-green-100 via-white to-green-200 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl md:rounded-3xl md:p-5"></div>
+    <div class="relative px-5 py-5 bg-white shadow-lg sm:rounded-3xl sm:p-5 md:rounded-3xl md:p-5">
           <img class="object-cover object-center rounded" alt="hero" src="https://www.nicepng.com/png/detail/332-3329966_the-woofin-paws-pet-fashion-show-starts-at.png"></img>
-        </div>
         </div>
         </div>
     </div>
   </div>
+  
+<header>
+    <nav class="bg-white shadow">
+        <div class="container mx-auto px-6 py-3 ">
+            <div class="md:flex md:items-center md:justify-between">
+                <div class="flex justify-between items-center">
+                    <div class="text-xl font-semibold text-gray-700">
+                        <a href="#" class="text-gray-800 text-xl font-bold hover:text-gray-700 md:text-2xl">Brand</a>
+                    </div>
+
+                   
+                    <div class="flex md:hidden">
+                        <button type="button" class="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600" aria-label="toggle menu">
+                            <svg viewBox="0 0 24 24" class="h-6 w-6 fill-current">
+                                <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+
+               
+                <div class="hidden -mx-4 md:flex md:items-center">
+                    <a href="#" class="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600">Web developers</a>
+                    <a href="#" class="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600">Web Designers</a>
+                    <a href="#" class="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600">UI/UX Designers</a>
+                    <a href="#" class="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600">Contact</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="w-full bg-cover bg-center" style={{height:'32rem',backgroundImage:"https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"}}>
+        <div class="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
+            <div class="text-center">
+                <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl">Build Your new <span class="underline text-blue-400">Saas</span></h1>
+                <button class="mt-4 px-4 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Start project</button>
+            </div>
+        </div>
+    </div>
+</header>
 </section>
                   
     )
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,6 +11,16 @@ const Navbar = (props) => {
           <img src="https://www.pngkit.com/png/full/24-240954_paw-logo-quiz-cat.png" class="w-12 h-12 text-white p-2 bg-gray-200 rounded-full" viewBox="0 0 24 24"/>
           <span class="ml-3 text-4xl font-abril">Meowy Shop</span>
         </a>
+        <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center font-cuerpo">
+          <a class="mr-5 hover:text-blue-400">Inicio</a>
+          <a class="mr-5 hover:text-blue-400">Cuidados</a>
+          <a class="mr-5 hover:text-blue-400">Productos</a>
+          <a class="mr-5 hover:text-blue-400">Contacto</a>
+        </nav>
+        <div class="flex justify-center">
+        <button class="text-white bg-red-200 border-0 py-2 px-6 focus:outline-none hover:bg-red-300 rounded text-lg font-cuerpo  mt-4 md:mt-0">Regístrate</button>
+        <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg font-cuerpo  mt-4 md:mt-0">Iniciar Sesión</button>
+      </div>
         {/* <button class="inline-flex text-white bg-red-200 border-0 py-2 px-6 focus:outline-none hover:bg-red-300 rounded text-lg">
           Inicia Sesión
         </button> */}
